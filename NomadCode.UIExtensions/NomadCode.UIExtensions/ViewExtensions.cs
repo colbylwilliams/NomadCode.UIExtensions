0cdb1e79c1332185437496f9a1bd5fe5094e8797
Nate Rickard <nate.rickard@microsoft.com>
2017-05-17 16:37:00 -0400

    spaces -> tabs

--- NomadCode.UIExtensions/NomadCode.UIExtensions/ViewExtensions.cs
+++ NomadCode.UIExtensions/NomadCode.UIExtensions/ViewExtensions.cs
@@ -6,75 +6,75 @@ using UIKit;
 
 namespace NomadCode.UIExtensions
 {
-    public static class ViewExtensions
-    {
-        const int statusBarViewTag = 987;
+	public static class ViewExtensions
+	{
+		const int statusBarViewTag = 987;
 
-        public static void ConstrainToParentCenter (this UIView view, nfloat width = default (nfloat), nfloat height = default (nfloat))
-        {
-            if (view?.Superview == null) throw new InvalidOperationException ("Must add view to a superview before calling this method");
+		public static void ConstrainToParentCenter (this UIView view, nfloat width = default (nfloat), nfloat height = default (nfloat))
+		{
+			if (view?.Superview == null) throw new InvalidOperationException ("Must add view to a superview before calling this method");
 
-            view.TranslatesAutoresizingMaskIntoConstraints = false;
+			view.TranslatesAutoresizingMaskIntoConstraints = false;
 
-            var verticalFormat = string.Format ("V:[super]-(<=1)-[view{0}]", height == default (nfloat) ? string.Empty : $"({height})");
+			var verticalFormat = string.Format ("V:[super]-(<=1)-[view{0}]", height == default (nfloat) ? string.Empty : $"({height})");
 
-            var horizontalFormat = string.Format ("H:[super]-(<=1)-[view{0}]", width == default (nfloat) ? string.Empty : $"({width})");
+			var horizontalFormat = string.Format ("H:[super]-(<=1)-[view{0}]", width == default (nfloat) ? string.Empty : $"({width})");
 
-            var viewsAndMetrics = new object [] { "super", view.Superview, "view", view };
+			var viewsAndMetrics = new object [] { "super", view.Superview, "view", view };
 
-            view.Superview.AddConstraints (NSLayoutConstraint.FromVisualFormat (verticalFormat, NSLayoutFormatOptions.AlignAllCenterX, viewsAndMetrics));
+			view.Superview.AddConstraints (NSLayoutConstraint.FromVisualFormat (verticalFormat, NSLayoutFormatOptions.AlignAllCenterX, viewsAndMetrics));
 
-            view.Superview.AddConstraints (NSLayoutConstraint.FromVisualFormat (horizontalFormat, NSLayoutFormatOptions.AlignAllCenterY, viewsAndMetrics));
-        }
-
-
-        public static void AddStatusBarView (this UIViewController controller, UIColor backgroundColor)
-        {
-            var statusBarView = new UIView
-            {
-                Tag = statusBarViewTag,
-                BackgroundColor = backgroundColor,
-                TranslatesAutoresizingMaskIntoConstraints = false
-            };
-
-            controller.View.AddSubview (statusBarView);
-
-            controller.View.AddConstraints (NSLayoutConstraint.FromVisualFormat (@"H:|[statusBarView]|", 0, "statusBarView", statusBarView));
-            controller.View.AddConstraints (NSLayoutConstraint.FromVisualFormat (@"V:|[statusBarView(20.0)]", 0, "statusBarView", statusBarView));
-        }
+			view.Superview.AddConstraints (NSLayoutConstraint.FromVisualFormat (horizontalFormat, NSLayoutFormatOptions.AlignAllCenterY, viewsAndMetrics));
+		}
 
 
-        public static void UpdateStatusBarView (this UIViewController controller, UITraitCollection traitCollection)
-        {
-            var statusBarView = controller.View.ViewWithTag (statusBarViewTag);
+		public static void AddStatusBarView (this UIViewController controller, UIColor backgroundColor)
+		{
+			var statusBarView = new UIView
+			{
+				Tag = statusBarViewTag,
+				BackgroundColor = backgroundColor,
+				TranslatesAutoresizingMaskIntoConstraints = false
+			};
 
-            if (statusBarView != null)
-            {
-                statusBarView.Hidden = traitCollection.VerticalSizeClass == UIUserInterfaceSizeClass.Compact;
-            }
-        }
+			controller.View.AddSubview (statusBarView);
 
-
-        /// <summary>
-        /// Adds a border to this UIView's layer with the specified UIColor and width.
-        /// </summary>
-        /// <param name="color">The border color.</param>
-        /// <param name="width">The border width.</param>
-        public static void AddBorder(this UIView view, UIColor color, int width = 1)
-        {
-            view.Layer.BorderWidth = width;
-            view.Layer.BorderColor = color.CGColor;
-        }
+			controller.View.AddConstraints (NSLayoutConstraint.FromVisualFormat (@"H:|[statusBarView]|", 0, "statusBarView", statusBarView));
+			controller.View.AddConstraints (NSLayoutConstraint.FromVisualFormat (@"V:|[statusBarView(20.0)]", 0, "statusBarView", statusBarView));
+		}
 
 
-        /// <summary>
-        /// Removes the border from this UIView's layer.
-        /// </summary>
-        public static void RemoveBorder(this UIView view)
-        {
-            view.Layer.BorderWidth = 0;
-        }
-    }
+		public static void UpdateStatusBarView (this UIViewController controller, UITraitCollection traitCollection)
+		{
+			var statusBarView = controller.View.ViewWithTag (statusBarViewTag);
+
+			if (statusBarView != null)
+			{
+				statusBarView.Hidden = traitCollection.VerticalSizeClass == UIUserInterfaceSizeClass.Compact;
+			}
+		}
+
+
+		/// <summary>
+		/// Adds a border to this UIView's layer with the specified UIColor and width.
+		/// </summary>
+		/// <param name="color">The border color.</param>
+		/// <param name="width">The border width.</param>
+		public static void AddBorder (this UIView view, UIColor color, int width = 1)
+		{
+			view.Layer.BorderWidth = width;
+			view.Layer.BorderColor = color.CGColor;
+		}
+
+
+		/// <summary>
+		/// Removes the border from this UIView's layer.
+		/// </summary>
+		public static void RemoveBorder (this UIView view)
+		{
+			view.Layer.BorderWidth = 0;
+		}
+	}
 }
 
 #endif
